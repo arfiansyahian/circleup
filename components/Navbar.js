29154e5a5e70b1b38852432262a32029513d89f8
CircleUp Dev <dev@circleup.local>
2026-09-16 02:48:29 +0000

    Pasang logo asli CircleUp di Navbar dan favicon

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,11 @@
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <div className="navbar">
-      <Link href="/" className="logo">
+      <Link href="/" className="logo" style={{ display: "flex", alignItems: "center", gap: 10 }}>
+        <Image src="/logo.png" alt="CircleUp" width={36} height={36} style={{ borderRadius: "50%" }} />
         CIRCLE UP
       </Link>
       <Link href="/login" className="muted">
